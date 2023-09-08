69ca8589fa21b5fe378d6a6966a06d1043815f6b
lawrence <kofiarkoh0@gmail.com>
2023-09-08 07:47:22 +0000

    add login handler

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,22 +1,46 @@
 "use client";
-import React, {PropsWithChildren} from "react";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
-import InstaIcon from "@/assets/images/insta.svg";
-import CatIcon from "@/assets/images/cat.svg";
 import BookIcon from "@/assets/images/book.svg";
+import CatIcon from "@/assets/images/cat.svg";
+import InstaIcon from "@/assets/images/insta.svg";
 import YoutubeIcon from "@/assets/images/tube.svg";
+import Box from "@mui/material/Box";
 import {Formik, FormikHelpers} from "formik";
+import {useRouter} from "next/navigation";
+import {PropsWithChildren} from "react";
 import * as Yup from "yup";
 import FormTextField from "../../../components/form/FormTextField";
 import SubmitButton from "../../../components/form/SubmitButton";
+import {useLazyGetFavouriteCatsQuery} from "../../../redux/rtk/catsApi";
+import {useAppDispatch} from "../../../redux/store";
+import {updateToken} from "../../../redux/tokenSlice";
 
 const valdiationSchema = Yup.object().shape({
 	apikey: Yup.string().required(),
 });
+const initialData = {apikey: ""};
+
 export default function Page() {
-	const handleLogin = () => {};
+	const [getFavouriteCats, {isLoading}] = useLazyGetFavouriteCatsQuery();
+	const dispatch = useAppDispatch();
+	const router = useRouter();
+
+	const handleLogin = (
+		data: typeof initialData,
+		helpers: FormikHelpers<typeof initialData>
+	) => {
+		dispatch(updateToken(data.apikey));
+		getFavouriteCats({})
+			.unwrap()
+			.then((response) => {
+				localStorage.setItem("api_key", data.apikey);
+				router.push("/cats");
+			})
+			.catch((error) => {
+				console.log(error);
+				helpers.setErrors({apikey: "Invalid API key"});
+			});
+	};
+
 	return (
 		<div
 			style={{
@@ -54,7 +78,7 @@ export default function Page() {
 					</IconRow>
 				</div>
 				<Formik
-					initialValues={{apikey: ""}}
+					initialValues={initialData}
 					validationSchema={valdiationSchema}
 					validateOnBlur={false}
 					validateOnMount={false}
@@ -72,6 +96,7 @@ export default function Page() {
 						<div>
 							<SubmitButton
 								variant="contained"
+								loading={isLoading}
 								sx={{
 									mt: 4,
 								}}>
